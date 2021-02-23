4e866be297d1f489591b97c0bd0f00d1ac92f43c
kennet0 <kennet2240@gmail.com>
2021-02-23 11:35:21 +0900

    -

---
+++ PythonImageProcess/14_opencv_threshold.py
@@ -0,0 +1,24 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+address = 'PythonImageProcess/files/'
+image =cv2.imread(address+'gray_image.jpg', cv2.IMREAD_GRAYSCALE)
+
+images = []
+ret, thres1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+ret, thres2 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thres3 = cv2.threshold(image, 127, 255, cv2.THRESH_TRUNC)
+ret, thres4 = cv2.threshold(image, 127, 255, cv2.THRESH_TOZERO)
+ret, thres5 = cv2.threshold(image, 127, 255, cv2.THRESH_TOZERO_INV)
+
+images.append(thres1)
+images.append(thres2)
+images.append(thres3)
+images.append(thres4)
+images.append(thres5)
+
+for i in images:
+    plt.imshow(cv2.cvtColor(i, cv2.COLOR_GRAY2RGB))
+    plt.show()
+
